d2ebc018bf37f6bdaf375966e77c3a613c4b14cd
jainish7-tech <jainish17ty@gmail.com>
2026-04-23 15:01:57 +0530

    adding python files

---
+++ swap.py
@@ -0,0 +1,9 @@
+a=int(input("enter the  first no="))
+b=int(input("enter the second no="))
+print("before swapping")
+print("value of a",a," value of b",b)
+x=a
+a=b
+b=x
+print("after swapping:")
+print("value of a",a," value of b",b)
